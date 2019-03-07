7f2c0f2d48127d7f44bb35c0c9a71a14732fa4b4
misteakes <simplywanderlust@aol.com>
2019-03-06 21:55:35 -0500

    what is happening lol why won't the pictures pop up :-(

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,46 @@
 //scripts will go here.
 
+document.getElementById("modal-btn").addEventListener("click", modalOpenFunction);
+
+function modalOpenFunction() {
+    var modal = document.getElementById("modal");
+    modal.classList.add("modal--open");
+}
+
+document.getElementById("modal-close").addEventListener("click", modalCloseFunction);
+
+function modalCloseFunction() {
+    var modal = document.getElementById("modal");
+    modal.classList.remove("modal--open");
+}
+
+document.getElementById("theme-btn").addEventListener("click", themeFunction);
+
+function themeFunction() {
+    var root = document.documentElement;
+
+    root.style.setProperty('--primary-color', '#000000');
+    root.style.setProperty('--secondary-color', '#ffffff');
+    root.style.setProperty('opacity', '.5');
+}
+
+document.getElementById("paragraph-btn").addEventListener("click", paragraphFunction);
+
+function paragraphFunction() {
+    var description = document.getElementsByClassName("description")[0];
+    // var newParagraph = document.createElement("p");
+    // var paragraphText = document.createTextNode("Additional paragraph text");
+
+    // newParagraph.appendChild(paragraphText);
+    // description.appendChild(newParagraph);
+
+    // description.innerHTML = "we just changed the text";
+
+    // description.innerHTML = Date();
+}
+
+document.getElementById("cursor-btn").addEventListener("click", cursorFunction);
+
+function cursorFunction() {
+    document.body.style.cursor = "help";
+}
