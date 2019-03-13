55e1a51ebb5de3223a30cb33c35fe4d5539801cf
bajrushivlora <bajrushivlora@gmail.com>
2019-03-12 20:57:00 -0400

    wow

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,42 @@
 //scripts will go here.
 
+document.getElementById("modal-btn").addEventListener("click", modalOpenFunction);
+
+function modalOpenFunction() {
+    var modal = document.getElementById("modal");
+    modal.classList.add("modal--open");
+}
+
+document.getElementById("modal-close").addEventListener("click", modalCloseFunction);
+
+function modalCloseFunction() {
+    var modal = document.getElementById("modal");
+    modal.classList.remove("modal--open");
+}
+
+document.getElementById("theme-btn").addEventListener("click", themeFunction);
+
+function themeFunction() {
+    var root = document.documentElement;
+
+    root.style.setProperty('--primary-color', '#000000');
+    root.style.setProperty('--secondary-color', '#FFFFFF');
+    root.style.setProperty('opacity', '0.5');
+}
+
+document.getElementById("paragraph-btn").addEventListener("click", paragraphFunction);
+
+function paragraphFunction() {
+    var description = document.getElementsByClassName("description")[0];
+    var newParagraph = document.createElement("p");
+    var paragraphText = document.createTextNode("Additional paragraph text");
+
+    newParagraph.appendChild(paragraphText);
+    description.appendChild(newParagraph);
+}
+
+document.getElementById("cursor-btn").addEventListener("click", cursorFunction);
+
+function cursorFunction() {
+    document.body.style.cursor = "help";
+}
